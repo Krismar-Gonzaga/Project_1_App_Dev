e4e3a2c24d044be2041af600e76aa6ba82fde214
Mik <119164057+Kyropyro@users.noreply.github.com>
2025-02-26 22:50:50 +0800

    Update SavingsAccountLauncher.java

--- Project 1/Accounts/SavingsAccountLauncher.java
+++ Project 1/Accounts/SavingsAccountLauncher.java
@@ -1,25 +1,77 @@
-public class SavingsAccountLauncher {
-    
-    public void savingsAccountInit() {
-        // Implementation for initializing a savings account
+package Accounts;
+
+
+/**
+ * Class for launching savings account-related operations.
+ */
+public class SavingsAccountLauncher extends AccountLauncher {
+    /**
+     * Constructor for SavingsAccountLauncher.
+     *
+     * @param bank The bank associated with this launcher.
+     */
+    public SavingsAccountLauncher(Bank bank) {
+        super(bank);
     }
 
-    private void depositProcess() {
-        // Implementation for deposit process
+    /**
+     * Handles the deposit process for a savings account.
+     *
+     * @param amount The amount to deposit.
+     * @param account The account to receive the deposit.
+     */
+    public void depositProcess(double amount, Account account) {
+        // Implementation pending
     }
 
-    private void withdrawProcess() {
-        // Implementation for withdraw process
+    /**
+     * Handles the withdrawal process for a savings account.
+     *
+     * @param amount The amount to withdraw.
+     * @param account The account from which to withdraw.
+     */
+    public void withdrawProcess(double amount, Account account) {
+        // Implementation pending
     }
 
-    private void fundTransferProcess() {
-        // Implementation for fund transfer process
+    /**
+     * Handles the fund transfer process between accounts.
+     *
+     * @param amount The amount to transfer.
+     * @param fromAccount The account sending the funds.
+     * @param toAccount The account receiving the funds.
+     */
+    public void fundTransferProcess(double amount, Account fromAccount, Account toAccount) {
+        // Implementation pending
+    }
+        @Override
+    public Account getLoggedAccount() {
+        return null;
     }
 
-    protected SavingsAccount getLoggedAccount() {
-        // Implementation for getting logged-in savings account
-        return new SavingsAccount();
+    @Override
+    public void setBank(Bank bank) {
+
+    }
+
+    @Override
+    public Bank getBank() {
+        return null;
+    }
+
+    @Override
+    public boolean isLogged() {
+        return false;
+    }
+
+    @Override
+    public void setAccount(Account account) {
+
+    }
+
+    @Override
+    public boolean checkCredentials(String accountNumber, String pin) {
+        return false;
     }
 }
-
 
