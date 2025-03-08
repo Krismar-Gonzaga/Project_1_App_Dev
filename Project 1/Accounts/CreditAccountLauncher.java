f82cd96597f8e3a0f1252f6eabda270d5e94d686
Mik <119164057+Kyropyro@users.noreply.github.com>
2025-03-08 23:23:16 +0800

    Update CreditAccountLauncher.java

--- Project 1/Accounts/CreditAccountLauncher.java
+++ Project 1/Accounts/CreditAccountLauncher.java
@@ -1,19 +1,27 @@
 package Accounts;
 import Banks.*;
 
-
-
 public class CreditAccountLauncher extends AccountLauncher {
-
     public CreditAccountLauncher(Bank bank, Account account) {
         super(bank, account);
     }
     
+    /**
+     * Method that deals with all things about credit accounts. Mainly utilized for showing the main
+     * menu after Credit Account users log in to the application.
+     */
     public void creditAccountInit() {
-       
+        CreditAccount account = getLoggedAccount();
+        if (account != null) {
+            System.out.println("Credit Account Menu");
+            System.out.println("Account Number: " + account.getAccountNumber());
+            System.out.println("Current Balance: " + account.getBalance());
+            System.out.println("Available Credit: " + account.getAvailableCredit());
+        } else {
+            System.out.println("No credit account is currently logged in.");
+        }
     }
-
-
+    
     public void creditRepaymentProcess() {
         // Implementation pending
     }
@@ -24,6 +32,10 @@ public class CreditAccountLauncher extends AccountLauncher {
 
   
     protected CreditAccount getLoggedAccount() {
-       return null;
+       Account account = super.getLoggedAccount();
+        if (account instanceof CreditAccount) {
+            return (CreditAccount) account;
+        }
+        return null;
     }
 }
