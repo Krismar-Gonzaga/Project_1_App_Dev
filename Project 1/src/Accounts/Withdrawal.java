1bb3f9ac42eeed5c1580d31b4c021283f79bbb1d
Mik <119164057+Kyropyro@users.noreply.github.com>
2025-03-22 21:18:18 +0800

    comments

--- Project 1/src/Accounts/Withdrawal.java
+++ Project 1/src/Accounts/Withdrawal.java
@@ -1,5 +1,9 @@
 package Accounts;
-
+/**
+ * The {@code Withdrawal} interface defines the contract for objects that can perform withdrawal operations.
+ * Any class that implements this interface must provide an implementation for the withdrawal method.
+ * This interface is part of the Accounts package and supports the banking system's transaction capabilities.
+ */
 public interface Withdrawal {
     boolean withdrawal(double amount);
 }
